f4252bef90171c6702b14a5ca68d2d97395b3bbc
refael_saidov <refaelsaidov1@gmail.com>
2025-11-10 11:12:58 +0200

    add import

--- user.py
+++ user.py
@@ -1,9 +1,12 @@
+from book import Book
+
 class User:
     def __init__(self,name, id, borrowed_books:list [Book] ):
         self.name=name
         self.id=id
         self.borrowed_books=borrowed_books
+
     def __str__(self):
-        print(f"the name is:{self.name}\n His ID card is:{self.id}\n borrowed_books is:{self.borrowed_books}")
+        return f"the name is:{self.name}\n His ID card is:{self.id}\n borrowed_books is:{self.borrowed_books}"
 
 
